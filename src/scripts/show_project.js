e5e1fcac90054fdceaef6eac6866271778206259
Daniël E <daniel.eekhout8@gmail.com>
2026-09-23 12:02:18 +0200

    added function to toggle text content for the project + styling

---
+++ src/scripts/show_project.js
@@ -0,0 +1,13 @@
+const projectContext = document.querySelector(".project-context");
+const button = document.querySelector("button");
+
+projectContext.style.display = "none";
+
+button.addEventListener("click", () => {
+  if (projectContext.style.display === "none") {
+    projectContext.style.display = "block";
+    button.textContent = "See less";
+  } else {
+    projectContext.style.display = "none";
+    button.textContent = "See more";
+}});
